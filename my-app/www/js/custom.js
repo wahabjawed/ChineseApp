c3a7d2dedbfd05d13bcef58d5dd5e0735af8c36a
Wahab Jawed <wahabjawed@gmail.com>
2014-06-12 15:08:22 +0500

    Few Changes

---
+++ my-app/www/js/custom.js
@@ -0,0 +1,10 @@
+  $(document).ready(function () {
+
+       		
+			 $('#ad').click(function () {
+              
+			  var ref = window.open('http://www.womingbai.com/en/survival-chinese/', '_self', 'location=no');
+			  
+            });
+			
+        });
---
+++ my-app/www/js/socialMedia.js
@@ -0,0 +1,26 @@
+  $(document).ready(function () {
+
+            $('#btnFB').click(function () {
+				
+				var ref = window.open('http://mobile.dudamobile.com/site/apc_1apc?url=http%3A%2F%2Fapc.com.ng%2F#2634', '_self', 'location=no');
+               
+            });
+            $('#btnTwitter').click(function () {
+              
+			  var ref = window.open('https://m.facebook.com/APCNigeria?_rdr', '_self', 'location=no');
+			  
+            });
+			$('#btnGoogle').click(function () {
+            
+			var ref = window.open('https://m.facebook.com/profile.php?id=1425841740973785', '_self', 'location=no');  
+            
+			});
+			
+     		$('#btnPintrest').click(function () {
+            
+			var ref = window.open('https://m.facebook.com/profile.php?id=1425841740973785', '_self', 'location=no');  
+            
+			});
+			
+			
+        });
