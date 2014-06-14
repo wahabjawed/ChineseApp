e7c1c034e7537e21649993a5438586b4e140ced4
Wahab Jawed <wahabjawed@gmail.com>
2014-06-14 16:48:47 +0500

    Revert "commit"
    This reverts commit 63fa80f34255b91a4e700a73a912bfc899e4e262.

---
+++ my-app/www/js/socialMedia.js
@@ -0,0 +1,26 @@
+  $(document).ready(function () {
+
+            $('#btnFB').click(function () {
+				
+				var ref = window.open('https://www.facebook.com/womingbai.en', '_self', 'location=no');
+               
+            });
+            $('#btnTwitter').click(function () {
+              
+			  var ref = window.open('https://twitter.com/Womingbai', '_self', 'location=no');
+			  
+            });
+			$('#btnGoogle').click(function () {
+            
+			var ref = window.open('https://plus.google.com/u/0/b/114448320882663893906/114448320882663893906/posts?hl=en', '_self', 'location=no');  
+            
+			});
+			
+     		$('#btnPintrest').click(function () {
+            
+			var ref = window.open('http://www.pinterest.com/WoMingbai/', '_self', 'location=no');  
+            
+			});
+			
+			
+        });
