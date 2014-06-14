a2be71e57801bc16bd28009e990588a25db651dd
Wahab Jawed <wahabjawed@gmail.com>
2014-06-14 16:52:32 +0500

    native

--- my-app/www/js/socialMedia.js
+++ my-app/www/js/socialMedia.js
@@ -2,23 +2,23 @@
 
             $('#btnFB').click(function () {
 				
-				var ref = window.open('https://www.facebook.com/womingbai.en', '_self', 'location=no');
+				var ref = window.open('https://www.facebook.com/womingbai.en', '_system', 'location=no');
                
             });
             $('#btnTwitter').click(function () {
               
-			  var ref = window.open('https://twitter.com/Womingbai', '_self', 'location=no');
+			  var ref = window.open('https://twitter.com/Womingbai', '_system', 'location=no');
 			  
             });
 			$('#btnGoogle').click(function () {
             
-			var ref = window.open('https://plus.google.com/u/0/b/114448320882663893906/114448320882663893906/posts?hl=en', '_self', 'location=no');  
+			var ref = window.open('https://plus.google.com/u/0/b/114448320882663893906/114448320882663893906/posts?hl=en', '_system', 'location=no');  
             
 			});
 			
      		$('#btnPintrest').click(function () {
             
-			var ref = window.open('http://www.pinterest.com/WoMingbai/', '_self', 'location=no');  
+			var ref = window.open('http://www.pinterest.com/WoMingbai/', '_system', 'location=no');  
             
 			});
 			
